8e73da162e672914d62fbaecfa5846d385c02476
HenriqueBertoIdi <164797248+HenriqueBertoIdi@users.noreply.github.com>
2026-05-02 21:21:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo da Velha</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="script.js">
+</head>
+<body>
+    <div class="main-container">
+        <div class="game-container">
+            <h1>Jogo da Velha</h1>
+            <hr>
+            <div class="info-jogador">
+                <label for="jogador1">Nome do Jogador 1:</label>
+                <input type="text" id="jogador1">
+                <label for="jogador2">Nome do Jogador 2:</label>
+                <input type="text" id="jogador2">
+                <button id="iniciar-jogo">Iniciar Jogo</button>
+                <button id="jogar-computador">Jogar contra o Computador</button>
+            </div>
+            <div class="container-jogo">
+                <div class="jogo">
+                    <div class="jogador-atual">JOGADOR DA VEZ:</div>
+                    <div class="tabuleiro">
+                        <button data-i="0"></button>
+                        <button data-i="1"></button>
+                        <button data-i="2"></button>
+                        <button data-i="3"></button>
+                        <button data-i="4"></button>
+                        <button data-i="5"></button>
+                        <button data-i="6"></button>
+                        <button data-i="7"></button>
+                        <button data-i="8"></button>
+                    </div>
+                </div>
+                <div class="ranking">
+                    <h2>Ranking</h2>
+                    <ul id="ranking"></ul>
+                </div>
+                <button id="zerar-ranking">Zerar Ranking</button>
+                <button id="voltar-menu">Voltar ao Menu Principal</button>
+            </div>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
